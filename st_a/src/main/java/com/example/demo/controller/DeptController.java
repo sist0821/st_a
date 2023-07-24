582648554ea9a237fedc13391890cd689b98cb4d
Your NameKimYEONSUASDADWQCEQWCEQEQCECQWE <rladustn50@nate.com>
2023-07-24 16:30:29 +0900

    부서등록 완료

--- st_a/src/main/java/com/example/demo/controller/DeptController.java
+++ st_a/src/main/java/com/example/demo/controller/DeptController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dao.DeptDAO_JPA;
 import com.example.demo.dao.DeptDAO_MB;
+import com.example.demo.entity.Dept;
 
 import lombok.Setter;
 
@@ -19,6 +21,18 @@ public class DeptController {
 	@Autowired
 	private DeptDAO_MB dao_MB;
 	
+	@GetMapping("/dept/insert")
+	public void insert() {
+	}
+	
+	
+	@PostMapping("/dept/insert")
+	public String insert(Dept d) {
+		dao_JPA.save(d);
+		return "redirect:/dept/list";
+	}
+	
+	
 	@GetMapping("/dept/list")
 	public void list(Model model) {
 		model.addAttribute("list", dao_JPA.findAll());
@@ -29,6 +43,8 @@ public class DeptController {
 		model.addAttribute("d", dao_MB.findByDno(dno));
 		return "/dept/detail";
 	}
+	
+	
 	
 }
 
